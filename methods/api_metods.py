0eda86894db9d47e34f0dc3c03f655b38924d3ac
Katsuba Igor <kasubaigor@gmail.com>
2024-09-15 18:33:42 +0300

    Файнал штрих по коду

--- methods/api_metods.py
+++ methods/api_metods.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import allure
 
 from path import path_resources, path_schemas
 
@@ -12,14 +13,16 @@ class Pet_api:
         response = requests.get(url=f'{base_url}pet/findByStatus/', params={'status': 'available'})
         with open(f'{path_schemas('get_by_status.json')}') as file:
             data_validate = json.load(file)
-        data_load = response.json()
+            with allure.step('Получение ответа метода post.findByStatus'):
+                data_load = response.json()
         return response, data_load, data_validate
 
     def pet_id_get(self, base_url, pet_id):
         response = requests.get(url=f'{base_url}pet/{pet_id}')
         with open(f'{path_schemas('get_by_id.json')}') as file:
             data_validate = json.load(file)
-        data_load = response.json()
+            with allure.step('Получение ответа метода get.pet'):
+                data_load = response.json()
         return response, data_load, data_validate
 
     def post_pet(self, base_url):
@@ -28,7 +31,8 @@ class Pet_api:
         response = requests.post(url=f'{base_url}pet/', json=data_upload)
         with open(f'{path_schemas('post_pet.json')}') as file:
             data_validate = json.load(file)
-        data_load = response.json()
+            with allure.step('Получение ответа метода post.pet'):
+                data_load = response.json()
         return response, data_load, data_validate
 
 
@@ -40,7 +44,8 @@ class Store_api:
         with open(f'{path_resources('post_store.json')}') as file:
             data_upload = json.load(file)
         response = requests.post(url=f'{base_url}store/order', json=data_upload)
-        data_load = response.json()
+        with allure.step('Получение ответа метода post.order'):
+            data_load = response.json()
         id_order = data_load["id"]
         with open(f'{path_schemas('post_store.json')}') as file:
             data_validate = json.load(file)
@@ -48,7 +53,8 @@ class Store_api:
 
     def delete_store(self, base_url, id_order):
         response = requests.delete(url=f'{base_url}store/order/{id_order}')
-        data_load = response.json()
+        with allure.step('Получение ответа метода delete.order'):
+            data_load = response.json()
         with open(f'{path_schemas('delete_store.json')}') as file:
             data_validate = json.load(file)
         return response, data_load, data_validate
